f00ce0b98296682128a21e1a0f143f576e86d299
FaraNisha <42019868+FaraNisha@users.noreply.github.com>
2019-03-13 11:39:47 +0700

    Add files via upload

---
+++ form-create.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="create.php" method="post">
+      Nama Dosen : <input type="text" name="nama_dosen"><br>
+      <br>
+      No. Telepon : <input type="text" name="telp"><br>
+      <br>
+      <input type="submit" name="btnSimpan" value="Simpan">
+    </form>
+  </body>
+</html>
---
+++ form-update.php
@@ -0,0 +1,40 @@
+<?php
+include '../connect.php';
+
+$id_dosen = $_GET['id_dosen'];
+
+$query = "SELECT * FROM dosen WHERE id_dosen = $id_dosen";
+
+$result = mysqli_query($connect, $query);
+
+$row = mysqli_connect_error($result);
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <title></title>
+   </head>
+   <body>
+     <form action="update.php?id_dosen=<?php echo $_GET['id_dosen']; ?>" method="post">
+       <table>
+         <tr>
+           <td><label for="nama">Nama Dosen</label></td>
+           <td>:</td>
+           <td><input value="<?php echo $row['nama_dosen']; ?>" type="text" name="nama_dosen" id="nama"></td>
+         </tr>
+         <tr>
+           <td><label for="nama">No. Telepon</label></td>
+           <td>:</td>
+           <td><input value="<?php echo $row['telp']; ?>" type="text" name="telp" id="no_telp"></td>
+         </tr>
+         <tr>
+           <td></td>
+           <td><input value="<?php echo $row['id_dosen']; ?>" type="hidden" name="id_dosen"></td>
+           <td><input type="submit" value="Simpan" name="btnSimpan"></td>
+         </tr>
+       </table>
+     </form>
+   </body>
+ </html>
